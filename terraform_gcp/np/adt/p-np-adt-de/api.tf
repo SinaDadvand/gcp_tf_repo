d83d659cbc7c75d558258460ee92601e4d051cad
sina.dadvand <sina.dadvand@gmail.com>
2026-08-19 16:03:16 +0000

    Enabling compute API for -adt-de

--- terraform_gcp/np/adt/p-np-adt-de/api.tf
+++ terraform_gcp/np/adt/p-np-adt-de/api.tf
@@ -37,3 +37,9 @@ resource "google_project_service" "cloudidentity_api_adt" {
   service            = "cloudidentity.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "compute_api" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
